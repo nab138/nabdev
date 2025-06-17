26bb9721e3d9e10cb6f316384705fd52c758dff8
nab138 <nab@nabdev.me>
2025-06-17 12:21:36 -0400

    Fill out contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,4 @@
+import "./Contact.css";
 import { useEffect } from "react";
 import { useTransitionNav } from "../utils/NavigationProvider";
 
@@ -8,5 +9,28 @@ export default function Contact() {
     finishTransition();
   }, [finishTransition]);
 
-  return <h1>Contact</h1>;
+  return (
+    <div className="contact-container">
+      <div>
+        <h1>Contact</h1>
+        <h2>I can't wait to hear from you!</h2>
+      </div>
+      <ul className="contact-list">
+        <li>
+          <b>Email</b>: <a href="mailto:nab@nabdev.me">nab@nabdev.me</a>
+        </li>
+        <li>
+          <b>GitHub</b>:{" "}
+          <a href="https://github.com/nab138">https://github.com/nab138</a>
+        </li>
+        <li>
+          <b>Chief Delphi</b>:{" "}
+          <a href="https://www.chiefdelphi.com/u/nab138">nab138</a>
+        </li>
+        <li>
+          <b>Discord</b>: nab138
+        </li>
+      </ul>
+    </div>
+  );
 }
